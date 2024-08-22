961b43e1b91d0222bc2ba6df3ccc96b2fda23ace
Shika <greffinroid@ymail.com>
2024-08-23 02:16:15 +0900

    Switched to mason

--- init.lua
+++ init.lua
@@ -1,26 +1,31 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    { "nvim-neo-tree/neo-tree.nvim",
-    	branch = "v3.x",
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
 			"MunifTanjim/nui.nvim"
 		}
-    },
+	},
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' }
+	},
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
@@ -77,9 +82,26 @@ require("lazy").setup({
 			})
 		end
 	},
-	{ "mfussenegger/nvim-jdtls" }
+	{ "williamboman/mason.nvim" },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig"
+		}
+	},
 })
 
-require('lspconfig').tsserver.setup{}
+require("lualine").setup()
+require("mason").setup()
+require("mason-lspconfig").setup {
+	ensure_installed = {
+		"cssls",
+		"gopls",
+		"html",
+		"jdtls",
+		"jsonls",
+		"tsserver",
+	}
+}
 
 require("configs")
