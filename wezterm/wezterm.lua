a7d27e899ef2e168d33088cb5ef74332b2c8d30f
Shika <greffinroid@ymail.com>
2025-07-15 02:19:10 +0900

    Use custom colors
    Wofi style

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,11 +9,38 @@ config.hide_tab_bar_if_only_one_tab = true
 -- config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
 
-config.color_scheme = "Ayu Dark (Gogh)"
+-- config.color_scheme = "Ayu Dark (Gogh)"
 
 -- config.font_dirs = { "fonts" }
 -- config.font = wezterm.font("SauceCodePro Nerd Font Mono")
 -- config.font = wezterm.font("SpaceMono Nerd Font")
 config.font = wezterm.font("DepartureMono Nerd Font Mono")
+
+config.colors = {
+	background = "#0a0e19",
+	foreground = "#a3aecc",
+	cursor_bg = "#f7ff99",
+	cursor_fg = "#0a0e19",
+	ansi = {
+		"#0a0e19",
+		"#ff6d65",
+		"#f2ff65",
+		"#65ff6d",
+		"#65f3ff",
+		"#6d65ff",
+		"#ff65f3",
+		"#a3aecc",
+	},
+	brights = {
+		"#2d364c",
+		"#ff9f99",
+		"#f7ff99",
+		"#99ff9f",
+		"#99f8ff",
+		"#9f99ff",
+		"#ff99f8",
+		"#ffffff",
+	},
+}
 
 return config
