384c05c507ab6476ecb7a048b63a407b80bada4c
Shika <greffinroid@ymail.com>
2025-07-16 22:17:40 +0900

    Add load wallpaper from extra folder

--- hypr/scripts/random_wallpaper.sh
+++ hypr/scripts/random_wallpaper.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-loaded=$(hyprctl hyprpaper listloaded)
-arg=$(find ~/Pictures/wallpapers -type f | shuf -n 1)
+wallpaper_folder=~/Pictures/wallpapers
 
-hyprctl hyprpaper reload ", $arg"
+current_wallpaper=$(hyprctl hyprpaper listactive | grep eDP-1 | cut -d " " -f 3)
+
+exclude_arg="-path $wallpaper_folder/extra -prune -o"
+
+if [[ $# -gt 0  && $1 == '-e' ]] then
+	wallpaper_folder=$wallpaper_folder/extra
+	unset exclude_arg
+fi
+
+wallpaper=$(find $wallpaper_folder $exclude_arg -type f ! -name $(basename $current_wallpaper) -print | shuf -n 1)
+
+hyprctl hyprpaper reload ", $wallpaper"
 
