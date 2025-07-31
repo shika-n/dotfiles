68f542d345f76f3cca23266582830238cb0da7b2
Shika <greffinroid@ymail.com>
2025-07-31 20:29:01 +0900

    Switch hyprpaper to swww
    Update random wallpaper script to use 3 folders

--- hypr/scripts/random_wallpaper.sh
+++ hypr/scripts/random_wallpaper.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
 
-wallpaper_folder=~/Pictures/wallpapers
+base_folder="$HOME/Pictures/wallpapers/originals"
+folders=("base" "extra" "extra_plus")
 
-current_wallpaper=$(hyprctl hyprpaper listactive | grep eDP-1 | cut -d " " -f 3)
+current_wallpaper=$(swww query | grep eDP-1 | cut -d " " -f 5)
 
-exclude_arg="-path $wallpaper_folder/extra -prune -o"
+$ignore_args="-name _scaled -prune -o"
 
-if [[ $# -gt 0  && $1 == '-e' ]] then
-	wallpaper_folder=$wallpaper_folder/extra
-	unset exclude_arg
+using_folder="$base_folder/${folders[0]}"
+if [[ $# -gt 0 ]] then
+	if [[ $1 == '-e' ]] then
+		using_folder="$base_folder/${folders[1]}"
+	elif [[ $1 == '-r' ]]; then
+		using_folder="$base_folder/${folders[2]}"
+	fi
 fi
 
-wallpaper=$(find $wallpaper_folder $exclude_arg -type f ! -name $(basename $current_wallpaper) -print | shuf -n 1)
+wallpaper=$(find $using_folder $ignore_args -type f ! -name $(basename $current_wallpaper) -print | shuf -n 1)
 
-hyprctl hyprpaper reload ", $wallpaper"
+scaled="${wallpaper%.*}_scaled.png"
+if [[ -f $scaled ]] then
+	wallpaper=$scaled
+fi
+
+swww img $wallpaper --transition-type center --transition-duration 2
 
