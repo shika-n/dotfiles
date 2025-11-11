9b0bd76b5ed7bbee90f30348e068102f5ea5a469
Shika <greffinroid@ymail.com>
2025-11-11 15:35:21 +0900

    Disable automatic plugin update check

--- nvim/lua/configs/lazy.lua
+++ nvim/lua/configs/lazy.lua
@@ -11,13 +11,13 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 		lazypath
 	})
 	if vim.v.shell_error ~= 0 then
-	vim.api.nvim_echo({
-		{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-		{ out, "WarningMsg" },
-		{ "\nPress any key to exit..." },
-	}, true, {})
-	vim.fn.getchar()
-	os.exit(1)
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out,                            "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
 	end
 end
 vim.opt.rtp:prepend(lazypath)
@@ -28,9 +28,8 @@ require("lazy").setup({
 	spec = {
 		{ import = "plugins" },
 	},
-	checker = { enabled = true },
+	-- checker = { enabled = true },
 	ui = {
 		border = "double",
 	}
 })
-
