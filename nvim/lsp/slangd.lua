2e9c334b87816914e6982fd8537fca6b8513d9ea
Shika <greffinroid@ymail.com>
2025-12-03 09:15:32 +0900

    Add slangd lsp config for windows

---
+++ nvim/lsp/slangd.lua
@@ -0,0 +1,18 @@
+if vim.fn.has("win32") then
+	-- slangd uses CreateProcess which doesn't use PATHEXT to run mason's .cmd
+	-- files. Giving the full path to mason's slang .cmd file also doesn't work
+	-- since if clangFormatLocation contains path separator, slang will check if
+	-- the ending contains .exe. If not, it will append .exe so we ended up
+	-- with /mason/bin/clang-format.cmd.exe as the final path. Fortunately,
+	-- giving the name without path works since it's treated differently by
+	-- slang.
+	return {
+		settings = {
+			slang = {
+				format = {
+					clangFormatLocation = "clang-format.cmd",
+				},
+			},
+		},
+	}
+end
