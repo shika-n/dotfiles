cd65a78b7f099f2e8bc65044000f38c98f29a297
Shika <greffinroid@ymail.com>
2025-12-03 14:35:22 +0900

    Add vim globals library for auto complete

--- nvim/lsp/lua_ls.lua
+++ nvim/lsp/lua_ls.lua
@@ -1,10 +1,15 @@
 return {
 	settings = {
 		Lua = {
+			workspace = {
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+				},
+			},
 			diagnostics = {
-				globals = { "vim" }
-			}
-		}
-	}
+				globals = { "vim" },
+			},
+		},
+	},
 }
-
